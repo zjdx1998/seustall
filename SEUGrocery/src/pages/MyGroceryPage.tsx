2576f68c85eef7b526223fb5f14b864c4be64aa8
zjdx1998 <zjdx1998@gmail.com>
2019-08-28 02:06:10 +0800

    finish mac config and successfully run app in iPhone XR

--- SEUGrocery/src/pages/MyGroceryPage.tsx
+++ SEUGrocery/src/pages/MyGroceryPage.tsx
@@ -5,81 +5,91 @@
 */
 import React, {Component} from 'react';
 import {
-    ActivityIndicator,
-    View,
-    ScrollView,
-    StyleSheet,
-    Dimensions,
-    Button,
-    TouchableOpacity,
+  ActivityIndicator,
+  View,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  Button,
+  TouchableOpacity,
 } from 'react-native';
-import {
-    Text,
-    ThemeProvider,
-    Image,
-    Avatar,
-} from 'react-native-elements';
+import {Text, ThemeProvider, Image, Avatar} from 'react-native-elements';
 import GoodsPanel from '../Components/GoodsPanel';
-import LocalBackHeader from "../Components/LocalBackHeader";
-import MyGroceryHeader from "../Components/MyGroceryHeader";
+import LocalBackHeader from '../Components/LocalBackHeader';
+import MyGroceryHeader from '../Components/MyGroceryHeader';
 
 export default class MyGroceryPage extends Component {
-    private props: any;
-    render() {
-        return (
-            <ScrollView style={styles.baseContainer}>
-                <View style={styles.headerContainer}>
-                    <LocalBackHeader navigation={this.props.navigation} />
-                </View>
-                <View style={styles.headerContainer}>
-                    <MyGroceryHeader/>
-                </View>
-                <View style={styles.headerContainer} />
-                <View style={styles.selectContainer}>
-                    <TouchableOpacity   activeOpacity={0.2} focusedOpacity={0.5}>
-                        <View style=  {{justifyContent:'center',alignItems:'center',backgroundColor:'#ffffff'}}>
-                            <Text style={{color:'#cc6699'}}>全部</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity   activeOpacity={0.2} focusedOpacity={0.5}>
-                        <View style=  {{justifyContent:'center',alignItems:'center',backgroundColor:'#ffffff'}}>
-                            <Text style={{color:'#cc6699'}}>未卖出</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity   activeOpacity={0.2} focusedOpacity={0.5}>
-                        <View style=  {{justifyContent:'center',alignItems:'center',backgroundColor:'#ffffff'}}>
-                            <Text style={{color:'#cc6699'}}>已卖出</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.GoodsAreaContainer}>
-                    <GoodsPanel navigation={this.props.navigation} />
-                </View>
-            </ScrollView>
-        );
-    }
+  private props: any;
+  render() {
+    return (
+      <ScrollView style={styles.baseContainer}>
+        <View style={styles.headerContainer}>
+          <LocalBackHeader navigation={this.props.navigation} />
+        </View>
+        <View style={styles.headerContainer}>
+          <MyGroceryHeader />
+        </View>
+        <View style={styles.headerContainer} />
+        <View style={styles.selectContainer}>
+          <TouchableOpacity activeOpacity={0.2} focusedOpacity={0.5}>
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#ffffff',
+              }}>
+              <Text style={{color: '#cc6699'}}>全部</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.2} focusedOpacity={0.5}>
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#ffffff',
+              }}>
+              <Text style={{color: '#cc6699'}}>未卖出</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.2} focusedOpacity={0.5}>
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#ffffff',
+              }}>
+              <Text style={{color: '#cc6699'}}>已卖出</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.GoodsAreaContainer}>
+          <GoodsPanel navigation={this.props.navigation} />
+        </View>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    baseContainer: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: '#FFF0F5',
-    },
-    headerContainer: {
-        flex: 5,
-        backgroundColor:'#cc6699',
-    },
-    selectContainer: {
-        flex: 1,
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-    },
-    GoodsAreaContainer: {
-        flex: 20,
-    },
-    selectText:{
-        color: '#cc6699',
-    }
+  baseContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#FFF0F5',
+  },
+  headerContainer: {
+    flex: 5,
+    backgroundColor: '#cc6699',
+  },
+  selectContainer: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  GoodsAreaContainer: {
+    flex: 20,
+  },
+  selectText: {
+    color: '#cc6699',
+  },
 });
