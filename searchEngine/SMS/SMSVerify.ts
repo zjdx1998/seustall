823211575b2ae8171e18f6c65700b37fc8bf248a
zjdx1998 <zjdx1998@gmail.com>
2019-09-05 15:54:08 +0800

    modify sms server

--- searchEngine/SMS/SMSVerify.ts
+++ searchEngine/SMS/SMSVerify.ts
@@ -4,14 +4,12 @@
     @date: 2019/9/4
 */
 //npm install qcloudsms_js --save
-var sc = require('./secret')
+var sc = require('./secret.ts')
 var QCloudSMS = require("qcloudsms_js");
 var appID = sc.Decrypt("0DF115C3318934FCC9271317F1FE6E10");
-console.log(appID);
-console.log(sc.Encrypt("5d3689b4c39a53ce7526fdeace54d9e3"));
 var appKEY = sc.Decrypt("2C18DA9189DDC0BD6A55C50AB061779508685BD059D6E369BD2A5241578D24A661F919744954614304FAF53852524A2B");
 var templatedID = sc.Decrypt("6180D11E229A73FFBD56ED0F025EC99E");
-var smsSign = "东大杂货铺";
+var smsSign = '东大杂货铺';
 var qcloudsms = QCloudSMS(appID, appKEY);
 var ssender = qcloudsms.SmsSingleSender();
 function genCode(){
@@ -32,8 +30,10 @@ function callback(err, res, resData) {
 
 function SMSVerify(phoneNumber){
     let VerifyCode = genCode();
-    ssender(86, phoneNumber, templatedID, [VerifyCode], smsSign, "", "", callback);
+    ssender.sendWithParam(86, phoneNumber, templatedID, [VerifyCode], smsSign, "", "", callback);
     return VerifyCode;
 }
 
-module.exports = SMSVerify;
+module.exports = {
+    SMSVerify
+}
