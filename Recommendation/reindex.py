247689cffd2a70d14e8d3d256d21e91b4a43e82f
zjdx1998 <zjdx1998@gmail.com>
2019-09-11 01:10:29 +0800

    modify reindex

--- Recommendation/reindex.py
+++ Recommendation/reindex.py
@@ -11,14 +11,13 @@ conn = pymysql.connect(
 
 cursor = conn.cursor()
 
-sql = "select itemid, note, price, title from items"
-
 app = Flask(__name__)
 
 @app.route('/')
 def indexPage():
+    sql = "select itemid, note, price, title from items"
     cursor.execute(sql)
-    retItem = cursor.fetchmany(2)
+    retItem = cursor.fetchall()
     headers = {"Content-Type": "application/json"}
     for item in retItem:
         hosturl = "http://localhost:9200/index-goods/goods/" + str(item[0]) + '/_update?pretty'
@@ -33,23 +32,26 @@ def indexPage():
         }
         additemjson = json.dumps(additemjson)
         response = requests.post(hosturl, data=additemjson, headers = headers)
-    sql = "select uuid, note, price, title from items"
+    sql = "select uuid, address, idcard, info, score, studentid, username from users"
     cursor.execute(sql)
-    retItem = cursor.fetchmany(2)
+    retUser = cursor.fetchall()
     headers = {"Content-Type": "application/json"}
-    for item in retItem:
-        hosturl = "http://localhost:9200/index-goods/goods/" + str(item[0]) + '/_update?pretty'
-        additemjson = {
+    for user in retUser:
+        hosturl = "http://localhost:9200/index-users/users/" + str(user[0]) + '/_update?pretty'
+        adduserjson = {
             "doc": {
-                "note": item[1],
-                "price": item[2],
-                "title": item[3]
+                "address": user[1],
+                "idcard": user[2],
+                "info": user[3],
+                "score": user[4],
+                "studentid": user[5],
+                "username": user[6]
             },
             "doc_as_upsert": "true",
             "detect_noop": "false"
         }
-        additemjson = json.dumps(additemjson)
-        response = requests.post(hosturl, data=additemjson, headers = headers)
+        adduserjson = json.dumps(adduserjson)
+        response = requests.post(hosturl, data=adduserjson, headers = headers)
     return response.json()
 
 
--- SEUGrocery/App.tsx
+++ SEUGrocery/App.tsx
@@ -14,5 +14,4 @@ export default class App extends Component {
   render() {
     return <AppTotalNavigation />;
   }
-
 }
